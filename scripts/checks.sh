63b48f4bc9a5fa4cd3f60f8d80900d89d45a5ba5
NickTikhomirov <nicktikhomirov02@gmail.com>
2019-12-17 05:02:13 +0300

    chesk2

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,5 +2,5 @@
 set -e
 
 files=`find . -name "*.cpp" | grep -v "./tools/*" | grep -v "./third-party/*"`
-filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/line_length,-whitespace/comments,-build/include_order,-whitespace/tab,-whitespace/newline,-runtime/threadsafe_fn
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/line_length,-whitespace/comments,-build/include_order,-whitespace/tab,-whitespace/newline,-runtime/threadsafe_fn,-build/include_subdir
 echo $files | xargs cpplint --filter=$filter
